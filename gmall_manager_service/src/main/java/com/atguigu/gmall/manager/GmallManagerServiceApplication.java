f68b96d23b3344f6a2591300d28243ae7f9b0d99
tanyancheng <1518202665@qq.com>
2018-10-10 15:39:44 +0800

    所以工程

---
+++ gmall_manager_service/src/main/java/com/atguigu/gmall/manager/GmallManagerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallManagerServiceApplication.class, args);
+	}
+}
---
+++ gmall_user_web/src/main/java/com/atguigu/gmall/UserController.java
@@ -0,0 +1,23 @@
+package com.atguigu.gmall;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.movie.MovieService;
+import com.atguigu.gmall.user.Movie;
+import com.atguigu.gmall.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @Reference
+    MovieService movieService;
+
+    @RequestMapping("/movie")
+    public Movie buyTicket(String userId, String mId){
+        Movie movie = movieService.getMovie("1");
+        return movie;
+    }
+
+}
